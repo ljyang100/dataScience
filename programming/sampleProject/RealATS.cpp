199452682664e7267516af1c8b7ddd36b0e444e0
Lijun Yang <noone@nowhere.com>
2019-05-15 22:06:02 -0400

    Update RealATS.cpp

--- programming/sampleProject/RealATS.cpp
+++ programming/sampleProject/RealATS.cpp
@@ -37,8 +37,7 @@ int main( void )
 
 
 	//// Part II: back testing
-	////***** NOTE I NEED change the endDateIndex for symbol selection and testing.
-	//AllData a;  //Is it like a namespace, but with only one big container class a, and hence eliminate the namespace name. Furthermore with a short class name. In the namespace (class), there are many other container classes defined. So it looks fine.
+	//AllData a;  
 	//a.initializeTraderParameters();  a.initializeOtherParameters();
 	//a.m_backTest = true; //**** IMPORTANT! This must be false all the time for real trading.
 	//a.initializeSymbolVectors();
@@ -59,12 +58,6 @@ int main( void )
 	
 	
 	std::this_thread::sleep_for(std::chrono::hours(100)); //This basically keeps the thread always alive.
-	/////************** Don't comment this line
-	/////************** Don't comment this line
-	////*************** Two IMPORTANT Points: [1] If without the Sleep(100000000) below, then once the main thread is gone, then all other threads will be done. [2] When starting a new thread I need make sure that the class instance
-	////*************** used in the new thread will not be dead. The class instance should be alive all the time when the application is running, unless I really don't need the thread to be alive that long. In the main() above,
-	////*************** I define several class instances but not define them in a new class member function. This is because when that member function is gone, then class instance will often be gone too. If that happens, I will get errors such as "pure virtual funtion call " or "invalid access" or "unhandled exception" etc. 
-    // This is critically important for old C++ standard. Without it, the termination of main thread will terminate all other running threads. 
 	return 0;	
 	
 }
